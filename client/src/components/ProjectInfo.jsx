39a95d41f16d86246ae54becc446ddb80a2c243c
jon-zawada <jonathan_zawada@yahoo.com>
2020-10-10 17:45:45 -0700

    project info pages good css skeleton

--- client/src/components/ProjectInfo.jsx
+++ client/src/components/ProjectInfo.jsx
@@ -15,12 +15,16 @@ const ProjectInfo = ({ match }) => {
   const project = data[match.params.id];
   console.log(project);
   return (
-    <div className="project-info">
-      <img src={project.gif} alt="" className="img-info" />
-      <div className="text-info">
-        <h1 className="info-header">{project.name}</h1>
-        <h3>{project.description}</h3>
-        <h6>Technologies used: </h6>
+    <div className="project-info-wrapper">
+      <div className="project-info">
+        <img src={project.gif} alt="" className="img-info" />
+        <div className="text-info">
+          <h1 className="info-header">{project.name}</h1>
+          <h3>{project.description}</h3>
+          <h6>Technologies used: </h6>
+        </div>
+      </div>
+      <div className="list-wrapper">
         <ul className="bullet-list">
           {project.bullets.map((bullet) => (
             <li className="bullets">{bullet}</li>
