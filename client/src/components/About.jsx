ce4fe2c9468843b5d679242546a6604273197040
jon-zawada <jonathan_zawada@yahoo.com>
2020-10-20 11:32:34 -0700

    mobile view for about and projects final

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 const About = () => (
   <div id="about">
-    <img src="/images/selfie.png" alt="" />
+    <img src="/images/selfie.png" alt="" id="me" />
     <div id="about-text-wrapper">
       <p id="about-text">
         A year ago, a friend jokingly told me if you can solve a rubik's cube you can be a software enginner. Being an avid cuber and seeking my next challenge I put his theory to the test and found exceptional results.
