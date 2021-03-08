75a971b2a46ab56f206797a5397a83bd40ac282a
jon-zawada <jonathan_zawada@yahoo.com>
2021-03-08 13:24:41 -0800

    adds first step in setting up https

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = express();
-const port = 3001;
+const HTTP_PORT = 8080;
+const HTTPS_PORT = 8081;
+const port = 8080;
 const DIST_DIR = path.join(__dirname, '..', 'client', 'dist', 'index.html');
+const credentials = {
+  key: '',
+  cert: '',
+  ca: ''
+};
 
 app.use(express.static(path.join('client', 'dist')));
 
@@ -10,6 +20,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(DIST_DIR));
 });
 
-app.listen(port, () => {
-  console.log(`Portfolio listening on ${port}`);
+const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(HTTP_PORT, () => {
+  console.log(`Portfolio listening on ${HTTP_PORT} ---> mapped to 80`);
 });
+
+// httpsServer.listen(HTTPS_PORT, () => {
+//   console.log(`Portfolio listening on ${HTTPS_PORT} ---> mapped to 443`);
+// });
