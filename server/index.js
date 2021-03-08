910b26f39368bb9c409019caf57713afbe0d0df8
jon-zawada <jonathan_zawada@yahoo.com>
2021-03-08 13:46:55 -0800

    adds credentials for https

--- server/index.js
+++ server/index.js
@@ -8,10 +8,13 @@ const HTTP_PORT = 8080;
 const HTTPS_PORT = 8081;
 const port = 8080;
 const DIST_DIR = path.join(__dirname, '..', 'client', 'dist', 'index.html');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/jonzawada.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/jonzawada.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/jonzawada.com/chain.pem', 'utf8');
 const credentials = {
-  key: '',
-  cert: '',
-  ca: ''
+  key: privateKey,
+  cert: certificate,
+  ca: ca
 };
 
 app.use(express.static(path.join('client', 'dist'), { dotfiles: 'allow' }));
@@ -21,12 +24,12 @@ app.get('*', (req, res) => {
 });
 
 const httpServer = http.createServer(app);
-// const httpsServer = https.createServer(credentials, app);
+const httpsServer = https.createServer(credentials, app);
 
 httpServer.listen(HTTP_PORT, () => {
   console.log(`Portfolio listening on ${HTTP_PORT} ---> mapped to 80`);
 });
 
-// httpsServer.listen(HTTPS_PORT, () => {
-//   console.log(`Portfolio listening on ${HTTPS_PORT} ---> mapped to 443`);
-// });
+httpsServer.listen(HTTPS_PORT, () => {
+  console.log(`Portfolio listening on ${HTTPS_PORT} ---> mapped to 443`);
+});
